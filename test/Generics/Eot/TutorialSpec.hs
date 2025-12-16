49837c411846af3bce6ce40b07c2a6f9378e02f5
Sönke Hahn <soenkehahn@gmail.com>
2025-12-16 12:52:23 -0500

    Remove CPP for older ghc versions

--- test/Generics/Eot/TutorialSpec.hs
+++ test/Generics/Eot/TutorialSpec.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE CPP #-}
-
 module Generics.Eot.TutorialSpec where
-
-#if MIN_VERSION_base(4,9,0)
 
 import           Test.DocTest
 import           Test.Hspec
@@ -14,14 +10,3 @@ spec = describe "tutorial" $ do
 
   it "doctests" $ do
     doctest (words "test/Generics/Eot/Tutorial.lhs -isrc -pgmL markdown-unlit")
-
-#else
-
-import           Test.Hspec
-
-import           Generics.Eot.Tutorial ()
-
-spec :: Spec
-spec = return ()
-
-#endif
